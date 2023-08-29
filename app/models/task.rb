2615787e0e67f9953b72d56c06b96edb7823b558
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2023-08-29 20:30:10 +0100

    reworked project and task models to make use of active record

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,21 +1,15 @@
-class Task
-    attr_accessor :size, :completed_at
-    def initialize options = {}
-        mark_completed(options[:completed_at]) if options[:completed_at]
-        @size = options[:size]
-    end
+class Task < ApplicationRecord
+    belongs_to :project
     def mark_completed(date = Time.current)
-        @completed_at = date
+        self.completed_at = date
     end
     def complete?
-        @completed_at.present?
+        completed_at.present?
     end
-
     def part_of_velocity?
         return false unless complete?
-        completed_at > Project.velocity_length_in_days.days.ago    
+        completed_at > Project.velocity_length_in_days.days.ago
     end
-
     def points_toward_velocity
         part_of_velocity? ? size : 0
     end
